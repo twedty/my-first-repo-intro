c21e8f8ef3678cb0285cbd8ac3003cd8f12982f3
twedty <jamestwedt@gmail.com>
2024-01-23 22:03:47 -0500

    Added JS

---
+++ assets/index.js
@@ -0,0 +1,42 @@
+$("#show-button").click(function () {
+	$(".hours").fadeToggle("slow");
+});
+
+$("html").fadeIn(2000);
+
+$(function () {
+	$(".start")
+		.fadeIn("slow")
+		.delay(300)
+		.animate({ opacity: 1, left: "200px" }, 600);
+});
+$(function () {
+	$("#food-truck")
+		.fadeIn("slow")
+		.delay(300)
+		.animate({ opacity: 1, left: "200px" }, 600);
+});
+$(function () {
+	$(".title")
+		.fadeIn("slow")
+		.delay(300)
+		.animate({ opacity: 1, left: "200px" }, 600);
+});
+$(function () {
+	$(".about")
+		.fadeIn("slow")
+		.delay(300)
+		.animate({ opacity: 1, left: "200px" }, 600);
+});
+$(function () {
+	$(".menu")
+		.fadeIn("slow")
+		.delay(300)
+		.animate({ opacity: 1, left: "200px" }, 600);
+});
+$(function () {
+	$(".mathF")
+		.fadeIn("slow")
+		.delay(300)
+		.animate({ opacity: 1, left: "200px" }, 600);
+});
